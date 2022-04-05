1cf92525d5b9b202f948b932509c0e993df8dbb5
Mayuri <gmayuri@aienterprise.com>
2022-04-05 18:13:29 +0530

    testing purpose

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BaseLayout from './Layout/baseLayout';
+
+function App() {
+  return (
+    <div className="App">
+      <BaseLayout/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Layout/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className="site-footer">
+            <div className="container-fluid">
+                <p className='copyright'>&copy; 2022 <a href='#' >Footer Here </a>All Rights Reserved</p>
+            </div>
+        </footer>
+    );
+}
+export default Footer;
